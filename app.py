22b59e4d851596a3a4b40cc59ef57e6a82a3fc5b
Yash Trivedi <f20190834@goa.bits-pilani.ac.in>
2023-05-03 14:44:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, send_file
+from PIL import Image
+import io
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Enhance X-ray images right here!</title>
+</head>
+<body>
+  <h1>Enhance X-ray images right here!</h1>
+  <text>Simply upload your image from the device, and click on Process, and see the results on the same window!</text>
+  <br><br>
+  <form action="process_image" method="post" enctype="multipart/form-data">
+    <input type="file" name="image_file" accept="image/*"><br><br>
+    <input type="submit" value="Process">
+  </form>
+</body>
+</html>
+"""
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    img_bytes = request.files['image_file'].read()
+    image = Image.open(io.BytesIO(img_bytes))
+    img = np.array(image)
+    clahe=cv2.createCLAHE(clipLimit=3.0,tileGridSize=(5,5))
+    grey_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    clahe_img=clahe.apply(grey_img)
+    
+    
+    # Process the image using your Python function
+    processed_image =Image.fromarray(np.uint8(clahe_img))
+    processed_image.resize((300,300),resample=0)
+
+    
+    # Save the output image to a buffer
+    output_buffer = io.BytesIO()
+    processed_image.save(output_buffer, format='PNG')
+    output_buffer.seek(0)
+    
+    return send_file(output_buffer, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
